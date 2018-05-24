b007bb6dbb041ead7434998ad0b86b71ae097f0c
HPLegion <32222819+HPLegion@users.noreply.github.com>
2018-05-24 13:38:42 +0200

    Multiple Changes - Add some comments - Rename the filename generator function - Start implementing the ElectronBeam class

--- DR.py
+++ DR.py
@@ -3,7 +3,7 @@ This module contains classes and functions helping to simulate charge breeding i
 including effects of dielectronic recombination
 """
 import os
-import multiprocessing as mp
+# import multiprocessing as mp
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -22,12 +22,12 @@ EPS_0 = scipy.constants.epsilon_0
 K_B = scipy.constants.Boltzmann
 C_L = scipy.constants.speed_of_light
 
-M_E_EV = M_E * C_L**2 / Q_E
+M_E_EV = M_E * C_L**2 / Q_E # Electron mass in eV
 
 ##### Logic for robust file imports
 _MODULEDIR = os.path.dirname(os.path.abspath(__file__))
 _RESOURCEDIR = os.path.join(_MODULEDIR, "resources/")
-_getresdir = lambda f: os.path.join(_RESOURCEDIR, f)
+_GETRESDIR = lambda f: os.path.join(_RESOURCEDIR, f)
 
 ##### Helper functions for translating chemical symbols
 def _load_chemical_elements():
@@ -37,7 +37,7 @@ def _load_chemical_elements():
     _z = [] # Atomic Number
     _es = [] # Element Symbol
     _name = [] # Element Name
-    with open(_getresdir("./ChemicalElements.csv")) as f:
+    with open(_GETRESDIR("./ChemicalElements.csv")) as f:
         for line in f:
             data = line.split(",")
             _z.append(int(data[0]))
@@ -117,40 +117,110 @@ def plot_charge_state_evolution(ode_solution, xlim=(1e-4, 1e3), ylim=(1e-4, 1),
 #     return herrmann_radius(cur, e_kin, b, r_c, t_c, b_c)
 
 
-# ##### Class Definitions
-# class ElectronBeam:
-#     """
-#     This class contains logic that allows estimating the space charge corrected energy
-#     of an electron beam and the resulting energy spread
-#     """
-#     def __init__(b_0, r_d, r_c, t_c, b_c):
-#         """
-#         Set the constant machine parameters
+##### Class Definitions
+class ElectronBeam:
+    """
+    This class contains logic that allows estimating the space charge corrected energy
+    of an electron beam and the resulting energy spread
+    """
+    def __init__(self, b_d, r_d, b_c, r_c, t_c):
+        """
+        Set the constant machine parameters
 
-#         Input Parameters
-#         b_0 - magnetic flux density in the trap centre in Tesla
-#         r_d - drift tube radius in m
-#         r_c - cathode radius in m
-#         t_c - cathode temperatur in K
-#         b_c - magnetic flux density on the cathode surface
-#         """
-#         self.b_0 = b_0
-#         self.r_d = r_d
-#         self.r_c = r_c
-#         self.t_c = t_c
-#         self.b_c = b_c
+        Input Parameters
+        b_d - magnetic flux density in the trap centre in Tesla
+        r_d - drift tube radius in m
+        r_c - cathode radius in m
+        t_c - cathode temperatur in K
+        b_c - magnetic flux density on the cathode surface
+        """
+        self._b_d = b_d
+        self._r_d = r_d
+        self._r_c = r_c
+        self._t_c = t_c
+        self._b_c = b_c
 
-# class RexElectronBeam(ElectronBeam):
-#     """
-#     ElectronBeam Class with REXEBIS Parameters set by default
-#     """
-#     def __init__():
-#         b_0 = 2
-#         r_d = 5/1000
-#         r_c = .8/1000
-#         t_c = 1600
-#         b_c = .2
-#         super().__init__(b_0, r_d, r_c, t_c, b_c)
+    @staticmethod
+    def electron_velocity(e_kin):
+        """
+        Returns the electron velocity corresponding to a kin. energy in m/s
+
+        Input Parameters
+        e_kin - electron energy in eV
+        """
+        return C_L * np.sqrt(1 - (M_E_EV / (M_E_EV + e_kin))**2)
+
+    @staticmethod
+    def characteristic_potential(cur, e_kin):
+        """
+        Returns the characteristic potential due to spacecharge in V
+
+        Input Parameters
+        cur - electron beam current in A
+        e_kin - electron energy in eV
+        """
+        v = ElectronBeam.electron_velocity(e_kin)
+        return cur / (4 * PI * EPS_0 * v)
+
+    def herrmann_radius(self, cur, e_kin):
+        """
+        Returns the Hermann radius of an electron beam with the given machine parameters in m
+
+        Input Parameters
+        cur - electron beam current in A
+        e_kin - electron energy in eV
+        """
+        v = ElectronBeam.electron_velocity(e_kin)
+        s1 = M_E * cur / (PI * EPS_0 * Q_E * v * self._b_d**2)
+        s2 = 8 * K_B * self._t_c * M_E * self._r_c**2 / (Q_E**2 * self._b_d**2)
+        s3 = self._b_c**2 * self._r_c**4 / (self._b_d**2)
+        return np.sqrt(s1 + np.sqrt(s1**2 + s2 + s3))
+
+    def space_charge_correction(self, r, cur, e_kin):
+        """
+        Returns the space charge correction at a given radius r, current and electron beam energy
+        This is done by iteratively computing the spacecharge correction and the hermann radius
+        until the realtive change in space charge correction is < 1e-6
+
+        Input Parameters
+        r - distance from axis in m
+        cur - electron beam current in A
+        e_kin - electron energy in eV
+        """
+        if r > self._r_d or r < 0:
+            raise ValueError("r cannot be bigger than the drift tube radius or smaller than 0")
+
+        # Init iterative solution
+        sc_on_ax_new = 1
+        sc_on_ax_old = 0
+        while (sc_on_ax_new - sc_on_ax_old)/sc_on_ax_new > 1e-6: # Check relative difference
+            sc_on_ax_old = sc_on_ax_new
+            # Compute kinetic energy correction
+            corr_e_kin = e_kin + Q_E * sc_on_ax_old
+            # Compute Herrmann radius and characteristic potential
+            r_e = self.herrmann_radius(cur, corr_e_kin)
+            phi_0 = ElectronBeam.characteristic_potential(cur, e_kin)
+            # Compute space charge correction
+            sc_on_ax_new = phi_0 * (2 * np.log(r_e / self._r_d) - 1)
+        
+        # When loop is exited r_e and phi_0 should have the right values
+        if r < r_e:
+            multip = 2 * np.log(r_e / self._r_d) + (r / r_e)**2 - 1
+        else:
+            multip = 2 * np.log(r / self._r_d)
+        return phi_0 * multip
+
+class RexElectronBeam(ElectronBeam):
+    """
+    ElectronBeam Class with REXEBIS Parameters set by default
+    """
+    def __init__(self):
+        b_d = 2
+        r_d = 5/1000
+        r_c = .8/1000
+        t_c = 1600
+        b_c = .2
+        super().__init__(b_d, r_d, b_c, r_c, t_c)
 
 class LotzCrossSections:
     """
@@ -184,7 +254,7 @@ class LotzCrossSections:
         # list of lists where each sublist hold the energies for one charge state
         # self._e_bind[n] describes charge state n+
         self._e_bind = []
-        with open(_getresdir("./BindingEnergies/%d.txt" % self._z)) as fobj:
+        with open(_GETRESDIR("./BindingEnergies/%d.txt" % self._z)) as fobj:
             for line in fobj:
                 line = line.split()
                 line = [float(elem.strip()) for elem in line]
@@ -198,7 +268,7 @@ class LotzCrossSections:
         # list of lists where each sublist hold the configuration for on charge state
         # self._cfg[n] describes charge state n+
         self._cfg = []
-        with open(_getresdir("./BindingEnergies/%dconf.txt" % self._z)) as fobj:
+        with open(_GETRESDIR("./BindingEnergies/%dconf.txt" % self._z)) as fobj:
             for line in fobj:
                 line = line.split()
                 line = [int(elem.strip()) for elem in line]
@@ -334,7 +404,7 @@ class KLLCrossSections:
         Private Helper Method for loading the DR Data
         """
         # Convenient expression that generates the full path to the DR files in a robust manner
-        kll_path_gen = lambda x: _getresdir("./DRDataKLLGroundState/"
+        kll_path_gen = lambda x: _GETRESDIR("./DRDataKLLGroundState/"
                                             + self._es + "." + x + ".dr.txt")
         # Load files
         he_like = self._import_KLL_block(kll_path_gen("He"))
@@ -580,7 +650,7 @@ class SimpleEBISProblem:
         """
         The ode system describing the charge breeding
         """
-        j = self._j / scipy.constants.elementary_charge
+        j = self._j / Q_E
         dydt = self._species.LotzCrossSections.cross_section_matrix(self._e_kin).dot(y)
         dydt += self._species.KLLCrossSections.cross_section_matrix(self._e_kin, self._fwhm).dot(y)
         dydt *= j
@@ -648,7 +718,7 @@ class ContinuousNeutralInjectionEBISProblem(SimpleEBISProblem):
         """
         The ode system describing the charge breeding
         """
-        j = self._j / scipy.constants.elementary_charge
+        j = self._j / Q_E
         dydt = self._species.LotzCrossSections.cross_section_matrix(self._e_kin).dot(y)
         dydt += self._species.KLLCrossSections.cross_section_matrix(self._e_kin, self._fwhm).dot(y)
         dydt *= j
