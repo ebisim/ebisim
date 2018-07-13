691b2efb001a8cd96e86eb13b2bfe9a35c00e02f
HPLegion <32222819+HPLegion@users.noreply.github.com>
2018-07-13 10:41:38 +0200

    refactor resource path method

--- ebisim/utils.py
+++ ebisim/utils.py
@@ -7,11 +7,20 @@ import os
 ##### Logic for robust file imports
 _MODULEDIR = os.path.dirname(os.path.abspath(__file__))
 _RESOURCEDIR = os.path.join(_MODULEDIR, "resources/")
-_GETRESDIR = lambda f: os.path.join(_RESOURCEDIR, f)
+
+def _get_res_path(fn):
+    """
+    Generates the path to a filename in the resource folder
+    
+    fn - filename
+    """
+    return os.path.join(_RESOURCEDIR, fn)
 
 def open_resource(fn):
     """
     Method for opening files in the resource folder in a robust way without worrying
     about the absolute path
+
+    fn - filename
     """
-    return open(_GETRESDIR(fn))
+    return open(_get_res_path(fn))
