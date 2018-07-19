fd6acafafe21bf585a8b71306e85ced65c14ab56
HPLegion <32222819+HPLegion@users.noreply.github.com>
2018-07-19 10:44:50 +0200

    load all elements that should comprise the standard interface of ebisim

--- ebisim/__init__.py
+++ ebisim/__init__.py
@@ -2,3 +2,9 @@
 This module contains classes and functions helping to simulate charge breeding in an EBIS
 including effects of dielectronic recombination
 """
+
+from .xs import EBISSpecies
+from .problems import SimpleEBISProblem, ContinuousNeutralInjectionEBISProblem, EnergyScan
+from .physconst import *
+from .elements import ChemicalElement
+from . import beams
