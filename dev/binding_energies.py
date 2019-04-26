f5fd121ed3724a888e46cdbde229bf3f98f920fb
HPLegion <32222819+HPLegion@users.noreply.github.com>
2019-04-26 18:07:25 +0200

    Add more understandable output

--- dev/binding_energies.py
+++ dev/binding_energies.py
@@ -15,7 +15,7 @@ import time
 
 # X.txt contains binding energies for the different (sub)shells for element(Z=X)
 # Xconf.txt contains (sub)shell occupation
-# 1s 2s 2p- 2p+ 3s 3p- 3p+ 3d- 3d+ 4s 4p- 4p+ 4d- 4d+ 5s 5p- 5p+ 4f- 4f+ 5d- 5d+ 6s 6p- 6p+ 5f- 5f+ 6d- 6d+ 7s 
+# 1s 2s 2p- 2p+ 3s 3p- 3p+ 3d- 3d+ 4s 4p- 4p+ 4d- 4d+ 5s 5p- 5p+ 4f- 4f+ 5d- 5d+ 6s 6p- 6p+ 5f- 5f+ 6d- 6d+ 7s
 # 0  1  2   3   4  5   6   7   8   9  10  11  12  13  14 15  16  17  18  19  20  21 22  23  24  25  26  27  28
 
 # the readme is missing (for the case z=103 Lr)
@@ -101,22 +101,27 @@ def load_energies(z):
 
 
 def main():
+    print("binding_energies.py running...")
+
     out = {}
     for z in range(1, 106):
         data = {}
         data["ebind"] = load_energies(z)
         data["conf"] = load_conf(z)
         out[str(z)] = data # json keys are always strings
-    
+
+    # Write file
     with open("../ebisim/resources/BindingEnergies.json", "w") as f:
         json.dump(out, f)
-    
+
+    # Check if json load correctly restores the output
     start = time.time()
     with open("../ebisim/resources/BindingEnergies.json", "r") as f:
         val = json.load(f)
     print(f"Loading took {time.time() - start} s.")
 
-    print("Valid" if out == val else "Invalid")
+    print("json.load() valid" if out == val else "json.load() invalid")
+    print("binding_energies.py done.")
 
 if __name__ == "__main__":
     main()
