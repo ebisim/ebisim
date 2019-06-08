5e05be6909bf014b372a4a1f0c961cc1bb1abf59
Hannes Pahl <32222819+HPLegion@users.noreply.github.com>
2019-06-08 12:47:57 +0200

    Implement ComplexEBISProblem as function advanced_simulation

--- ebisim/simulation.py
+++ ebisim/simulation.py
@@ -6,6 +6,7 @@ import sys
 import numpy as np
 import pandas as pd
 import scipy.integrate
+# import numba
 
 from . import plotting
 from . import xs
@@ -299,6 +300,166 @@ def basic_simulation(element, j, e_kin, t_max,
     return Result(param=param, t=sol.t, N=sol.y)
 
 
+def advanced_simulation(element, j, e_kin, t_max,
+                        dr_fwhm=None, N_kbT_initial=None, adv_param=None,
+                        solver_kwargs=None):
+    """
+    !!!UNDER ACTIVE DEVELOPMENT!!! - API not stable!
+
+    Interface for performing advanced charge breeding simulations.
+
+    These simulations only include the most important effects, i.e. electron ionisation,
+    radiative recombination and optionally dielectronic recombination (for those transitions whose
+    data is available in the resource directory). All other effects are not ignored.
+
+    Continuous Neutral Injection (CNI) can be activated on demand.
+
+    The results only represent the proportions of different charge states, not actual densities.
+
+    Parameters
+    ----------
+    element : ebisim.elements.Element or str or int
+        An instance of the Element class, or an identifier for the element, i.e. either its
+        name, symbol or proton number.
+    j : float
+        <A/cm^2>
+        Current density
+    e_kin : float
+        <eV>
+        Electron beam energy
+    t_max : float
+        <s>
+        Simulated breeding time
+    dr_fwhm : None or float, optional
+        <eV>
+        If a value is given, determines the energy spread of the electron beam
+        (in terms of Full Width Half Max) and hence the effective width of DR resonances.
+        Otherwise DR is excluded from the simulation. By default None.
+    N_kbT_initial : None or numpy.array, optional
+        <1/m^3 and eV>
+        Determines the initial charge state distribution and temperatures if given.
+        Must have 2(Z + 1) entries, where the first Z + 1 entries give the ion densities in
+        1/m^3 with the array index corresponding to the charge state and
+        the last Z + 1 entries describe the temperatures in eV.
+        If no value is given the distribution defaults to approx 100% of 1+ ions at t = 0 s with a
+        temperature of 0.5 eV.
+        By default None.
+    adv_param : None or dict, optional
+        This dict can be used to set advanced parameters of the simulation.
+    solver_kwargs : None or dict, optional
+        If supplied these keyword arguments are unpacked in the solver call.
+        Refer to the documentation of scipy.integrate.solve_ivp for more information.
+        By default None.
+
+    Returns
+    -------
+    ebisim.simulation.Result
+        An instance of the Result class, holding the simulation parameters, timesteps and
+        charge state distribution including the species temperature.
+    """
+    # TODO: verify docstring
+
+    # cast element to Element if necessary
+    if not isinstance(element, elements.Element):
+        element = elements.Element(element)
+
+    # set initial conditions if not supplied by user
+    if not N_kbT_initial:
+        N_kbT_initial = np.ones(2 * (element.z + 1))
+        N_kbT_initial[:element.z + 1] *= MINIMAL_DENSITY
+        N_kbT_initial[element.z + 1:] *= MINIMAL_KBT
+        N_kbT_initial[1] = 1e16
+        N_kbT_initial[element.z + 2] = 0.5
+
+    if adv_param is None:
+        adv_param = {}
+    adv_param.setdefault("Vtrap_ax", 300)
+    adv_param.setdefault("Vtrap_ra", 50)
+    adv_param.setdefault("B_ax", 2)
+    adv_param.setdefault("r_dt", 0.005)
+    adv_param.setdefault("bg_N0", 1e-7 / (0.025 * Q_E)) # 10e-10mbar at 300K
+    adv_param.setdefault("bg_IP", 21.56) # eV
+
+    # prepare solver options
+    if not solver_kwargs:
+        solver_kwargs = {}
+    solver_kwargs.setdefault("method", "Radau")
+
+    # save adjusted call parameters for passing on to Result
+    param = locals().copy()
+
+    # @numba.jit
+    def rhs(_, y):
+        ve = plasma.electron_velocity(e_kin)
+        je = j / Q_E * 1e4
+        Ne = je / ve
+        q = np.arange(element.z + 1)
+        A = element.a
+
+        ### Split y vector into density and temperature
+        N = y[:element.z + 1]
+        kbT = y[element.z + 1:]
+        # E = N * kbT # Not currently needed
+
+        # precompute collision rates
+        rij = plasma.ion_coll_rate_mat(N, N, kbT, kbT, A, A)
+        ri = np.sum(rij, axis=1)
+
+        ### Particle density rates
+        R_ei = je * N * xs.eixs_vec(element, e_kin)
+        R_rr = je * N * xs.rrxs_vec(element, e_kin)
+        if dr_fwhm is not None:
+            R_dr = je * N * xs.drxs_vec(element, e_kin, dr_fwhm)
+        else:
+            R_dr = np.zeros(element.z + 1)
+
+        sigcx = 1.43e-16 * q**1.17 * adv_param["bg_IP"]**-2.76
+        R_cx = N * adv_param["bg_N0"] * \
+            np.sqrt(8 * Q_E * np.clip(kbT, 0, None)/(PI * A * M_P)) * sigcx
+        R_ax = plasma.escape_rate_axial(N, kbT, ri, adv_param["Vtrap_ax"])
+        R_ra = plasma.escape_rate_radial(
+            N, kbT, ri, A, adv_param["Vtrap_ra"], adv_param["B_ax"], adv_param["r_dt"]
+            )
+
+        ### Energy density rates
+        S_ei = R_ei * kbT
+        S_rr = R_rr * kbT
+        S_dr = R_dr * kbT
+        S_cx = R_cx * kbT
+        S_ax = R_ax * (kbT + q * adv_param["Vtrap_ax"])
+        S_ra = R_ra * (kbT + q * (adv_param["Vtrap_ra"] + adv_param["r_dt"] * adv_param["B_ax"] * \
+                                  np.sqrt(2 * Q_E * np.clip(kbT, 0, None) / (3 * A *M_P))))
+        # Electron heating
+        S_eh = plasma.electron_heating_vec(N, Ne, kbT, e_kin, A)
+        # Energy transfer between charge states within same "species"
+        S_tr = plasma.energy_transfer_vec(N, N, kbT, kbT, A, A, rij)
+
+        ### Construct rhs for N (density)
+        R_tot = -(R_ei + R_rr + R_dr + R_cx) - (R_ax + R_ra)
+        R_tot[1:] += R_ei[:-1]
+        R_tot[:-1] += R_rr[1:] + R_dr[1:] + R_cx[1:]
+
+        ### Construct rates for energy density flow
+        S_tot = -(S_ei + S_rr + S_dr + S_cx) + S_eh + S_tr - (S_ax + S_ra)
+        S_tot[1:] += S_ei[:-1]
+        S_tot[:-1] += S_rr[1:] + S_dr[1:] + S_cx[1:]
+
+        ### Deduce temperature flow -> Integrating temperature instead of energy has proven more
+        ### numerically stable
+        Q_tot = (S_tot - kbT * R_tot) / N
+        Q_tot[N <= MINIMAL_DENSITY] = 0 ### Freeze temperature if density is low (-> not meaningful)
+
+        return np.concatenate((R_tot, Q_tot))
+
+    sol = scipy.integrate.solve_ivp(rhs, (0, t_max), N_kbT_initial, **solver_kwargs)
+    return Result(
+        param=param,
+        t=sol.t,
+        N=sol.y[:element.z + 1, :],
+        kbT=sol.y[element.z + 1:, :],
+        N_is_density=True
+        )
+
 class EnergyScan:
     """
     Class that provides a simple interface to perform a scan of the electron beam energy for
@@ -403,217 +564,4 @@ class EnergyScan:
         """
         title = f"Abundance of {self._element.latex_isotope()}$^{{{cs}+}}$"
         fig = plotting.plot_energy_time_scan(self.solution, cs, xlim=xlim, ylim=ylim, title=title)
-        return fig
-
-
-class ComplexEBISProblem:
-    """
-    class defining an EBIS charge breeding simulation and providing the interface to solve it
-    Time independent problems only
-
-    Solving is done depending on the latest setting for the kinetic energy so this could create
-    race conditions. Be a bit careful with that, when doing batch work.
-    """
-
-    def __init__(self, element, j, e_kin, fwhm):
-        """
-        Defines the general problem constants (current density, electron energy and spread)
-
-        Input parameters
-        element - Identifier of the element under investigation
-        j - current density in A / cm^2
-        e_kin - electron energy
-        fwhm - electron energy spread (for DR width)
-        """
-        if not isinstance(element, elements.Element):
-            element = elements.Element(element)
-        self._element = element
-        self._fwhm = fwhm
-        self._j = j * 1e4 # convert to A/m**2
-        self._e_kin = e_kin
-        self._solution = None
-        self._Vtrap_ax = 300
-        self._Vtrap_ra = 50
-        self._B_ax = 2
-        self._r_dt = 5e-3
-        self._bg_N0 = 1e-7 / (0.025 * Q_E) # 10e-10mbar at 300K
-        self._bg_IP = 21.56 # eV
-        #Default initial condition for solving the EBIS ODE System (all atoms in 1+ charge state)
-        self._default_initial = np.ones(2*(self._element.z + 1))
-        self._default_initial[:self._element.z + 1] *= MINIMAL_DENSITY
-        self._default_initial[self._element.z + 1:] *= MINIMAL_KBT
-        self._default_initial[1] = 1e16
-        self._default_initial[self._element.z + 2] = 0.5
-
-    @property
-    def solution(self):
-        """Returns the solution of the lates solve of this problem"""
-        return self._solution
-
-    @property
-    def e_kin(self):
-        """Returns the kinetic energy"""
-        return self._e_kin
-
-    @e_kin.setter
-    def e_kin(self, val):
-        """Set e_kin to new value and delete existing solution"""
-        if val != self._e_kin:
-            self._solution = None
-            self._e_kin = val
-
-    def _rhs(self, t, y):
-        """
-        The right hand side of the ODE system for the complex EBIS problem
-        """
-        del t # not currently needed
-        ### Electron beam stuff
-        e_kin = self.e_kin
-        ve = plasma.electron_velocity(e_kin)
-        je = self._j / Q_E
-        Ne = je / ve
-        q = np.arange(self._element.z + 1)
-        A = self._element.a
-
-        ### Split y vector into density and temperature
-        N = y[:self._element.z + 1]
-        kbT = y[self._element.z + 1:]
-        # E = N * kbT # Not currently needed
-
-        # precompute collision rates
-        rij = plasma.ion_coll_rate_mat(N, N, kbT, kbT, A, A)
-        ri = np.sum(rij, axis=1)
-
-        ### Particle density rates
-        R_ei = je * N * xs.eixs_vec(self._element, e_kin)
-        R_rr = je * N * xs.rrxs_vec(self._element, e_kin)
-        R_dr = je * N * xs.drxs_vec(self._element, e_kin, self._fwhm)
-
-        sigcx = 1.43e-16 * q**1.17 * self._bg_IP**-2.76
-        R_cx = N * self._bg_N0 * np.sqrt(8 * Q_E * np.clip(kbT, 0, None)/(PI * A * M_P)) * sigcx
-        R_ax = plasma.escape_rate_axial(N, kbT, ri, self._Vtrap_ax)
-        R_ra = plasma.escape_rate_radial(N, kbT, ri, A, self._Vtrap_ra, self._B_ax, self._r_dt)
-
-        ### Energy density rates
-        S_ei = R_ei * kbT
-        S_rr = R_rr * kbT
-        S_dr = R_dr * kbT
-        S_cx = R_cx * kbT
-        S_ax = R_ax * (kbT + q * self._Vtrap_ax)
-        S_ra = R_ra * (kbT + q * (self._Vtrap_ra + self._r_dt * self._B_ax * \
-                                  np.sqrt(2 * Q_E * np.clip(kbT, 0, None) / (3 * A *M_P))))
-        # Electron heating
-        S_eh = plasma.electron_heating_vec(N, Ne, kbT, e_kin, A)
-        # Energy transfer between charge states within same "species"
-        S_tr = plasma.energy_transfer_vec(N, N, kbT, kbT, A, A, rij)
-
-        ### Construct rhs for N (density)
-        R_tot = -(R_ei + R_rr + R_dr + R_cx) - (R_ax + R_ra)
-        R_tot[1:] += R_ei[:-1]
-        R_tot[:-1] += R_rr[1:] + R_dr[1:] + R_cx[1:]
-
-        ### Construct rates for energy density flow
-        S_tot = -(S_ei + S_rr + S_dr + S_cx) + S_eh + S_tr - (S_ax + S_ra)
-        S_tot[1:] += S_ei[:-1]
-        S_tot[:-1] += S_rr[1:] + S_dr[1:] + S_cx[1:]
-
-        ### Deduce temperature flow -> Integrating temperature instead of energy has proven more
-        ### numerically stable
-        Q_tot = (S_tot - kbT * R_tot) / N
-        Q_tot[N <= MINIMAL_DENSITY] = 0 ### Freeze temperature if density is low (-> not meaningful)
-
-        return np.concatenate((R_tot, Q_tot))
-
-    def solve(self, max_time, y0=None, **kwargs):
-        """
-        solves the charge state evolution up to the given time
-        An intital distribution can be provided, if None is given, all ions will start as 1+
-
-        Returns the ODE solution object and saves it in solution property for later access
-
-        Input Parameters
-        max_time - Integration maximum time
-        y0 - initial distribution (numpy vector where the python index = charge state)
-        **kwargs - are forwarded to the solver (scipy.integrate.solve_ivp)
-        """
-        if y0 is None:
-            y0 = self._default_initial
-        lb = np.ones(y0.size) * MINIMAL_DENSITY
-        lb[self._element.z + 1:] *= MINIMAL_KBT
-        method = kwargs.pop("method", "Radau")
-        sol = scipy.integrate.solve_ivp(self._rhs, [0, max_time], y0, method=method, **kwargs)
-        self._solution = sol
-        return Result(
-            param=None,
-            t=sol.t,
-            N=sol.y[:self._element.z + 1, :],
-            kbT=sol.y[self._element.z + 1:, :],
-            N_is_density=True
-            )
-
-    def plot_cs_evo(self):
-        """
-        After the problem has been solved, the charge state evolution can be plotted by calling
-        this function
-
-        Returns figure handle and does not call show()
-        """
-        if self.solution is None:
-            print("Error! Need to solve problem before plotting")
-        tmax = self.solution.t.max()
-        xlim = (1e-5, tmax)
-        title = f"{self._element.latex_isotope()} charge state evolution " \
-                f"($E_{{e}} = {self._e_kin:0.1f}$ eV, FWHM = {self._fwhm:0.1f} eV)"
-        t = self.solution.t
-        N = self.solution.y[:self._element.z + 1, :]
-        ylim = (0, N.sum(axis=0).max()*1.05)
-        ylabel = ("Density (m$^{-3}$)")
-        fig = plotting.plot_generic_evolution(t, N, xlim=xlim, ylim=ylim, ylabel=ylabel,
-                                              title=title, yscale="linear", plot_total=True)
-        return fig
-
-    def plot_energy_evo(self):
-        """
-        After the problem has been solved, the energy denisty evolution can be plotted by calling
-        this function
-
-        Returns figure handle and does not call show()
-        """
-        if self.solution is None:
-            print("Error! Need to solve problem before plotting")
-        tmax = self.solution.t.max()
-        xlim = (1e-5, tmax)
-        title = f"{self._element.latex_isotope()} energy density evolution " \
-                f"($E_{{e}} = {self._e_kin:0.1f}$ eV, FWHM = {self._fwhm:0.1f} eV)"
-        t = self.solution.t
-        E = self.solution.y[self._element.z + 1:, :] * self.solution.y[:self._element.z + 1, :]
-        ymin = 10**(np.floor(np.log10(E[:, 0].sum(axis=0)) - 1))
-        ymax = 10**(np.ceil(np.log10(E.sum(axis=0).max()) + 1))
-        ylim = (ymin, ymax)
-        ylabel = ("Energy density (eV / m$^{-3}$)")
-        fig = plotting.plot_generic_evolution(t, E, xlim=xlim, ylim=ylim, ylabel=ylabel,
-                                              title=title, plot_total=True)
-        return fig
-
-    def plot_temperature_evo(self):
-        """
-        After the problem has been solved, the temperature evolution can be plotted by calling
-        this function
-
-        Returns figure handle and does not call show()
-        """
-        if self.solution is None:
-            print("Error! Need to solve problem before plotting")
-        tmax = self.solution.t.max()
-        xlim = (1e-5, tmax)
-        title = f"{self._element.latex_isotope()} temperature evolution " \
-                f"($E_{{e}} = {self._e_kin:0.1f}$ eV, FWHM = {self._fwhm:0.1f} eV)"
-        t = self.solution.t
-        T = self.solution.y[self._element.z + 1:, :]
-        ymin = 0.01
-        ymax = 10**(np.ceil(np.log10(T.max()) + 1))
-        ylim = (ymin, ymax)
-        ylabel = ("Temperature (eV)")
-        fig = plotting.plot_generic_evolution(t, T, xlim=xlim, ylim=ylim, ylabel=ylabel,
-                                              title=title)
         return fig
