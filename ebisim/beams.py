156759f34c50ddd4793ea27f807ae16570c9a4b3
Hannes Pahl <32222819+HPLegion@users.noreply.github.com>
2019-06-10 18:07:42 +0200

    Clean up and doctsrings (#1)

--- ebisim/beams.py
+++ ebisim/beams.py
@@ -4,34 +4,37 @@ found in an electron beam ion source / trap.
 """
 
 import numpy as np
-from .physconst import C_L, Q_E, M_E_EV, PI, EPS_0, K_B, M_E
-
-def electron_velocity(e_kin):
-    """
-    Returns the electron velocity corresponding to a kin. energy in m/s
-
-    Input Parameters
-    e_kin - electron energy in eV
-    """
-    return C_L * np.sqrt(1 - (M_E_EV / (M_E_EV + e_kin))**2)
+from .physconst import Q_E, PI, EPS_0, K_B, M_E
+from .plasma import electron_velocity
 
 class ElectronBeam:
     """
     This class contains logic that allows estimating the space charge corrected energy
     of an electron beam and the resulting energy spread
+
+    Parameters
+    ----------
+    cur : float
+        <A>
+        Electron beam current.
+    b_d : float
+        <T>
+        Magnetic flux density in the centre (drift tubes) of the EBIS.
+    r_d : float
+        <m>
+        Drift tube radius.
+    b_c : float
+        <T>
+        Magentic flux density on the cathode surface
+    r_c : float
+        <m>
+        Cathode radius.
+    t_c : float
+        <K>
+        Cathode temperature.
+
     """
     def __init__(self, cur, b_d, r_d, b_c, r_c, t_c):
-        """
-        Set the constant machine parameters
-
-        Input Parameters
-        cur - The electron current in A
-        b_d - magnetic flux density in the trap centre in Tesla
-        r_d - drift tube radius in m
-        r_c - cathode radius in m
-        t_c - cathode temperatur in K
-        b_c - magnetic flux density on the cathode surface
-        """
         self._cur = cur
         self._b_d = b_d
         self._r_d = r_d
@@ -42,8 +45,7 @@ class ElectronBeam:
     @property
     def current(self):
         """
-        Can read and write a new current to the electron beam object, which will then be used
-        for future computations
+        Current of the electron beam.
         """
         return self._cur
 
@@ -54,20 +56,39 @@ class ElectronBeam:
 
     def characteristic_potential(self, e_kin):
         """
-        Returns the characteristic potential due to spacecharge in V
+        Returns the characteristic potential due to spacecharge
 
-        Input Parameters
-        e_kin - electron energy in eV
+        Parameters
+        ----------
+        e_kin : float
+            <eV>
+            Electron kinetic energy.
+
+        Returns
+        -------
+        float
+            <V>
+            Characteristic potential.
         """
         v = electron_velocity(e_kin)
         return self._cur / (4 * PI * EPS_0 * v)
 
     def herrmann_radius(self, e_kin):
         """
-        Returns the Hermann radius of an electron beam with the given machine parameters in m
+        Returns the Hermann radius of an electron beam with the given machine parameters
 
-        Input Parameters
-        e_kin - electron energy in eV
+        Parameters
+        ----------
+        e_kin : float
+            <eV>
+            Electron kinetic energy.
+
+        Returns
+        -------
+        float
+            <m>
+            Hermann radius.
+
         """
         v = electron_velocity(e_kin)
         s1 = M_E * self._cur / (PI * EPS_0 * Q_E * v * self._b_d**2)
@@ -79,11 +100,26 @@ class ElectronBeam:
         """
         Returns the space charge correction at a given radius r, current and electron beam energy
         This is done by iteratively computing the spacecharge correction and the hermann radius
-        until the realtive change in space charge correction is < 1e-6
+        until the realtive change in space charge correction is < 1e-6.
 
-        Input Parameters
-        e_kin - electron energy in eV
-        r - distance from axis in m, defaults to 0
+        Parameters
+        ----------
+        e_kin : float
+            <eV>
+            Electron kinetic energy.
+        r : float, optional
+            Distance from the axis at which to evaluate the correction, by default 0.
+
+        Returns
+        -------
+        float
+            <V>
+            Space charge correction.
+
+        Raises
+        ------
+        ValueError
+            If r is larger than the drift tube radius.
         """
         if r > self._r_d or r < 0:
             raise ValueError("r cannot be bigger than the drift tube radius or smaller than 0")
@@ -109,27 +145,3 @@ class ElectronBeam:
         else:
             multip = 2 * np.log(r / self._r_d)
         return phi_0 * multip
-
-class RexElectronBeam(ElectronBeam):
-    """
-    ElectronBeam Class with REXEBIS Parameters set by default
-
-    Magnetic Field Trap: 2 T
-    Magnetic Field Cathode: 0.2 T
-    Cathode Radius: 0.8 mm
-    Drift tube Radius: 5 mm
-    Cathode Temp: 1600 K
-    """
-    def __init__(self, cur):
-        """
-        Set the constant machine parameters
-
-        Input Parameters
-        cur - The electron current in A
-        """
-        b_d = 2
-        r_d = 5/1000
-        r_c = .8/1000
-        t_c = 1600
-        b_c = .2
-        super().__init__(cur, b_d, r_d, b_c, r_c, t_c)
