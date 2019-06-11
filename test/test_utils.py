8a3f839a7266f83b0c836cef8bbc79635de7095d
HPLegion <32222819+HPLegion@users.noreply.github.com>
2019-06-11 17:49:33 +0200

    Remove obsolete testcase

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,7 +3,3 @@ Tests for ebisim.utils
 """
 import ebisim.utils
 # import pytest
-
-def test_open_chemical_elements_csv():
-    with ebisim.utils.open_resource("ChemicalElements.csv"):
-        pass
