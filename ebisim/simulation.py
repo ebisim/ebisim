80551e66047a9d0b58b371ca1c64cf48a5ed58e3
HPLegion <32222819+HPLegion@users.noreply.github.com>
2019-10-31 18:31:10 +0100

    Redesign of advanced sim and introduction of new data types

--- ebisim/simulation.py
+++ ebisim/simulation.py
@@ -5,6 +5,7 @@ their results.
 
 from warnings import warn
 from multiprocessing.pool import Pool
+from collections import namedtuple
 import numpy as np
 import scipy.integrate
 import scipy.interpolate
@@ -13,7 +14,7 @@ from . import plotting
 from . import xs
 from . import elements
 from . import plasma
-from .physconst import Q_E, M_P, PI
+from .physconst import Q_E, M_P, PI, EPS_0
 from .physconst import MINIMAL_DENSITY, MINIMAL_KBT
 
 
@@ -31,7 +32,11 @@ _RATE_NAMES = dict(
     S_ax="Axial losses",
     S_ra="Radial losses",
     S_eh="Electron heating",
-    S_tr="Heat transfer"
+    S_tr="Heat transfer",
+    S_ih="Ionisation heating",
+    V_ii="Self collision rate",
+    V_it="Total collision rate",
+    Comp="Charge compensation"
 ) #: Rates names for plot annotation
 
 
@@ -124,11 +129,22 @@ class Result:
 
         """
         if self.param:
-            title = f"{self.param['element'].latex_isotope()} {stub.lower()} " \
-                    f"($j = {self.param['j']:0.1f}$ A/cm$^2$, " \
-                    f"$E_{{e}} = {self.param['e_kin']:0.1f}$ eV)"
-            if "fwhm" in self.param:
-                title = title[:-1] + f", FWHM = {self.param['fwhm']:0.1f} eV)"
+            if "target" in self.param:
+                target = self.param["target"]
+                element = target.element
+            else:
+                element = self.param["element"]
+            if "device" in self.param:
+                device = self.param["device"]
+                e_kin = device.e_kin
+                j = device.j
+            else:
+                e_kin = self.param["e_kin"]
+                j = self.param["j"]
+            title = f"{element.latex_isotope()} {stub.lower()} " \
+                    f"($j = {j:0.1f}$ A/cm$^2$, $E_{{e}} = {e_kin:0.1f}$ eV)"
+            if self.param.get("dr_fwhm", None) is not None:
+                title = title[:-1] + f", FWHM = {self.param['dr_fwhm']:0.1f} eV)"
         else:
             title = stub
         return title
@@ -292,7 +308,7 @@ class Result:
         rate = self.rates[rate_key]
 
         kwargs.setdefault("xlim", (1e-4, self.t.max()))
-        kwargs.setdefault("title", self._param_title(_RATE_NAMES[rate_key]))
+        kwargs.setdefault("title", self._param_title(_RATE_NAMES.get(rate_key, "unkown rate")))
         kwargs.setdefault("yscale", "linear")
         kwargs.setdefault("plot_total", True)
 
@@ -406,9 +422,31 @@ def basic_simulation(element, j, e_kin, t_max,
     return Result(param=param, t=res.t, N=res.y, res=res)
 
 
-def advanced_simulation(element, j, e_kin, t_max,
-                        dr_fwhm=None, N_kbT_initial=None, adv_param=None,
-                        solver_kwargs=None):
+Target = namedtuple("Target", ["element", "N", "kbT", "cni", "cx"])
+
+def get_gas_target(element, N, kbT=0.025, cni=True, cx=True):
+    # cast element to Element if necessary
+    if not isinstance(element, elements.Element):
+        element = elements.get_element(element)
+    _N = np.ones(element.z + 1) * MINIMAL_DENSITY
+    _kbT = np.ones(element.z + 1) * MINIMAL_KBT
+    _N[0] = N
+    _kbT[0] = kbT
+    return Target(element, _N, _kbT, cni, cx)
+
+def get_ion_target(element, N, kbT=0.025, q=1):
+    # cast element to Element if necessary
+    if not isinstance(element, elements.Element):
+        element = elements.get_element(element)
+    _N = np.ones(element.z + 1) * MINIMAL_DENSITY
+    _kbT = np.ones(element.z + 1) * MINIMAL_KBT
+    _N[q] = N
+    _kbT[q] = kbT
+    return Target(element, _N, _kbT, cni=False, cx=False)
+
+Device = namedtuple("Device", ["j", "e_kin", "current", "r_e", "v_ax", "v_ra", "b_ax", "r_dt"])
+
+def advanced_simulation(device, target, t_max, dr_fwhm=None, solver_kwargs=None):
     """
     !!!UNDER ACTIVE DEVELOPMENT!!! - API not stable!
 
@@ -463,26 +501,7 @@ def advanced_simulation(element, j, e_kin, t_max,
         An instance of the Result class, holding the simulation parameters, timesteps and
         charge state distribution including the species temperature.
     """
-    # cast element to Element if necessary
-    if not isinstance(element, elements.Element):
-        element = elements.get_element(element)
-
-    # set initial conditions if not supplied by user
-    if not N_kbT_initial:
-        N_kbT_initial = np.ones(2 * (element.z + 1))
-        N_kbT_initial[:element.z + 1] *= MINIMAL_DENSITY
-        N_kbT_initial[element.z + 1:] *= MINIMAL_KBT
-        N_kbT_initial[1] = 1e16
-        N_kbT_initial[element.z + 2] = 0.5
-
-    if adv_param is None:
-        adv_param = {}
-    adv_param.setdefault("Vtrap_ax", 300)
-    adv_param.setdefault("Vtrap_ra", 50)
-    adv_param.setdefault("B_ax", 2)
-    adv_param.setdefault("r_dt", 0.005)
-    adv_param.setdefault("bg_N0", 1e-7 / (0.025 * Q_E)) # 10e-10mbar at 300K
-    adv_param.setdefault("bg_IP", 21.56) # eV
+    N_kbT_initial = np.concatenate((target.N, target.kbT))
 
     # prepare solver options
     if not solver_kwargs:
@@ -492,50 +511,72 @@ def advanced_simulation(element, j, e_kin, t_max,
     # save adjusted call parameters for passing on to Result
     param = locals().copy()
 
+    # prepare rhs
+    ## electrons
+    ve = plasma.electron_velocity(device.e_kin)
+    je = device.j / Q_E * 1e4
+    Ne = je / ve
+    on_ax_sc = device.current/(4 * PI * EPS_0 * ve) * (2*np.log(device.r_e/device.r_dt)-1)
+    ## ions
+    element = target.element
+    q = np.arange(element.z + 1)
+    A = element.a
+    ## cross sections
+    eixs = xs.eixs_vec(element, device.e_kin)
+    rrxs = xs.rrxs_vec(element, device.e_kin)
+    cxxs = 1.43e-16 * q**1.17 * element.ip**-2.76
+    if dr_fwhm is not None:
+        drxs = xs.drxs_vec(element, device.e_kin, dr_fwhm)
+    else:
+        drxs = np.zeros(element.z + 1)
+
     def rhs(_, y, rates=None):
-        ve = plasma.electron_velocity(e_kin)
-        je = j / Q_E * 1e4
-        Ne = je / ve
-        q = np.arange(element.z + 1)
-        A = element.a
 
         ### Split y vector into density and temperature
         N = y[:element.z + 1]
         kbT = y[element.z + 1:]
         # E = N * kbT # Not currently needed
 
+        # Compensation
+        comp = np.sum(q*N)/Ne
+        mean_ion_heat = 1/2 * device.current/(4 * PI * EPS_0 * ve) * (1-comp)
+        v_trap_rad = -1 * on_ax_sc * (1-comp)
+        v_trap_ax = device.v_ax + comp * on_ax_sc
+
         # precompute collision rates
         rij = plasma.ion_coll_rate_mat(N, N, kbT, kbT, A, A)
         ri = np.sum(rij, axis=1)
 
         ### Particle density rates
-        R_ei = je * N * xs.eixs_vec(element, e_kin)
-        R_rr = je * N * xs.rrxs_vec(element, e_kin)
-        if dr_fwhm is not None:
-            R_dr = je * N * xs.drxs_vec(element, e_kin, dr_fwhm)
-        else:
-            R_dr = np.zeros(element.z + 1)
+        R_ei = je * N * eixs
+        R_rr = je * N * rrxs
+        R_dr = je * N * drxs
 
-        sigcx = 1.43e-16 * q**1.17 * adv_param["bg_IP"]**-2.76
-        R_cx = N * adv_param["bg_N0"] * \
-            np.sqrt(8 * Q_E * np.clip(kbT, 0, None)/(PI * A * M_P)) * sigcx
-        R_ax = plasma.escape_rate_axial(N, kbT, ri, adv_param["Vtrap_ax"])
-        R_ra = plasma.escape_rate_radial(
-            N, kbT, ri, A, adv_param["Vtrap_ra"], adv_param["B_ax"], adv_param["r_dt"]
-            )
+        if target.cx:
+            R_cx = N * N[0] * np.sqrt(8 * Q_E * np.clip(kbT, 0, None)/(PI * A * M_P)) * cxxs
+            R_cx[1] = R_cx[1] - np.sum(R_cx[2:])
+        else:
+            R_cx = np.zeros_like(R_ei)
+        R_ax = plasma.escape_rate_axial(N, kbT, ri, v_trap_ax)
+        R_ra = plasma.escape_rate_radial(N, kbT, ri, A, v_trap_rad, device.b_ax, device.r_dt)
 
         ### Energy density rates
         S_ei = R_ei * kbT
         S_rr = R_rr * kbT
         S_dr = R_dr * kbT
         S_cx = R_cx * kbT
-        S_ax = R_ax * (kbT + q * adv_param["Vtrap_ax"])
-        S_ra = R_ra * (kbT + q * (adv_param["Vtrap_ra"] + adv_param["r_dt"] * adv_param["B_ax"] * \
+        S_ax = R_ax * (kbT + q * v_trap_ax)
+        S_ra = R_ra * (kbT + q * (v_trap_rad + device.r_dt * device.b_ax * \
                                   np.sqrt(2 * Q_E * np.clip(kbT, 0, None) / (3 * A *M_P))))
         # Electron heating
-        S_eh = plasma.electron_heating_vec(N, Ne, kbT, e_kin, A)
+        S_eh = plasma.electron_heating_vec(N, Ne, kbT, device.e_kin, A)
         # Energy transfer between charge states within same "species"
         S_tr = plasma.energy_transfer_vec(N, N, kbT, kbT, A, A, rij)
+        # Ionisation heating
+        S_ih = np.zeros_like(S_eh)
+        S_ih[1:] = R_ei[:-1] * mean_ion_heat
+        S_ih[:-1] -= (R_rr + R_dr + R_cx)[1:] * mean_ion_heat
+
 
         ### Construct rhs for N (density)
         R_tot = -(R_ei + R_rr + R_dr + R_cx) - (R_ax + R_ra)
@@ -546,6 +587,10 @@ def advanced_simulation(element, j, e_kin, t_max,
         S_tot = -(S_ei + S_rr + S_dr + S_cx) + S_eh + S_tr - (S_ax + S_ra)
         S_tot[1:] += S_ei[:-1]
         S_tot[:-1] += S_rr[1:] + S_dr[1:] + S_cx[1:]
+
+        if target.cni:
+            R_tot[0] = 0
+            S_tot[0] = 0
 
         ### Deduce temperature flow -> Integrating temperature instead of energy has proven more
         ### numerically stable
@@ -567,6 +612,10 @@ def advanced_simulation(element, j, e_kin, t_max,
             rates["S_ra"] = S_ra
             rates["S_eh"] = S_eh
             rates["S_tr"] = S_tr
+            rates["S_ih"] = S_ih
+            rates["V_ii"] = np.diag(rij)
+            rates["V_it"] = ri
+            rates["Comp"] = comp
 
         return np.concatenate((R_tot, Q_tot))
 
