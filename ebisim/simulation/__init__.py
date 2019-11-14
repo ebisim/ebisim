d3d697966b28237c0c5d78e62ca7ae939a9ba82c
Hannes Pahl <32222819+HPLegion@users.noreply.github.com>
2019-11-14 14:09:04 +0100

    add __all__ to simulation for apidoc use

--- ebisim/simulation/__init__.py
+++ ebisim/simulation/__init__.py
@@ -3,6 +3,10 @@ This subpackage contains functions and classes provide an interface to run simul
 their results.
 """
 
+from ._result import Result
+
+from ._basic import basic_simulation
+
 from ._advanced import (
     advanced_simulation,
     Target,
@@ -11,11 +15,19 @@ from ._advanced import (
     Device
 )
 
-from ._basic import basic_simulation
-
-from ._result import Result
-
 from ._energyscan import(
     energy_scan,
     EnergyScanResult
 )
+
+__all__ = [
+    "Result",
+    "basic_simulation",
+    "advanced_simulation",
+    "Target",
+    "get_ion_target",
+    "get_gas_target",
+    "Device",
+    "energy_scan",
+    "EnergyScanResult"
+]
