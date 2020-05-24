9dc0dcb255ea465409ff0ecc7f3eaef51356a58e
Hannes Pahl <32222819+HPLegion@users.noreply.github.com>
2020-05-24 12:22:22 +0200

    Save progress on implementation of new model class

---
+++ ebisim/simulation/_modelbuilder.py
@@ -0,0 +1,145 @@
+"""
+This module contains a model builder. It can be used to generate the RHS of a charge breeding
+simulation including certain effects.
+"""
+
+from collections import namedtuple
+import numpy as np
+import scipy.integrate
+import scipy.interpolate
+import numba
+
+from .. import xs
+from .. import elements
+from .. import plasma
+from ..physconst import Q_E, M_P, PI, EPS_0
+from ..physconst import MINIMAL_DENSITY, MINIMAL_KBT
+from ._result import Result
+
+Target = namedtuple("Target", elements.Element._fields + ("N", "kbT", "cni", "cx"))
+Device = namedtuple("Device", ["j", "e_kin", "current", "r_e", "v_ax", "v_ra", "b_ax", "r_dt"])
+
+
+
+class AdvancedModel:
+    def __init__(self, device, targets, bg_gases=None):
+        # Bind parameters
+        self.device = device
+        self.targets = targets
+        self.bg_gases = bg_gases
+
+        # Determine array bounds for differential equation
+        self.lb = np.zeros(len(targets), dtype=np.int32)
+        self.mb = np.zeros(len(targets), dtype=np.int32)
+        self.ub = np.zeros(len(targets), dtype=np.int32)
+        offset = 0
+        for i, trgt in enumerate(targets):
+            self.lb[i] = offset
+            self.mb[i] = self.lb[i] + trgt.z + 1
+            self.ub[i] = self.mb[i] + trgt.z + 1
+            offset = self.ub[i]
+
+
+    def rhs(self, t, y):
+        # pylint: disable=bad-whitespace
+        # Preallocate output
+        dn_out = np.zeros(y.size//2)
+        dkT_out = np.zeros(y.size//2)
+
+        # Compute some electron beam quantities
+        je = self.device.j / Q_E # electron number current density
+        ve = plasma.electron_velocity(self.device.e_kin)
+        ne = je/ve # Electron density
+
+
+        # Loop through targets
+        for i, trgt in enumerate(self.targets):
+            # Extract relevant state from y
+            lb = self.lb[i]
+            mb = self.mb[i]
+            ub = self.ub[i]
+            n = y[lb:mb]
+            kT = y[mb:ub]
+            kT[kT < MINIMAL_KBT] = MINIMAL_KBT # Make sure T is positive
+
+            # and allocate arrays for rates and other useful stuff
+            dn = np.zeros_like(n)
+            dkT = np.zeros_like(kT)
+            q = np.arange(trgt.z + 1)
+            v_th = np.sqrt(8 * Q_E * kT/(PI * trgt.A * M_P))
+
+
+            # EI
+            eixs      = xs.eixs_vec(trgt, self.device.e_kin)
+            R_ei      = eixs * n * je
+            dn       -= R_ei
+            dn[1:]   += R_ei[:-1]
+            dkT[1:]  += R_ei[:-1] / n[1:] * (kT[:-1] - kT[1:])
+
+
+            # RR
+            rrxs      = xs.rrxs_vec(trgt, self.device.e_kin)
+            R_rr      = rrxs * n * je
+            dn       -= R_rr
+            dn[:-1]  += R_rr[1:]
+            dkT[:-1] += R_rr[1:] / n[:-1] * (kT[1:] - kT[:-1])
+
+
+            # DR
+            drxs      = xs.drxs_vec(trgt, self.device.e_kin, _DRFWHM)
+            R_dr      = drxs * n * je
+            dn       -= R_dr
+            dn[:-1]  += R_dr[1:]
+            dkT[:-1] += R_dr[1:] / n[:-1] * (kT[1:] - kT[:-1])
+
+
+            # CX
+            R_cx      = np.zeros_like(n)
+            for gas in self.bg_gases:
+                cxxs  = 1.43e-16 * q**1.17 * gas.ip**-2.76
+                R_cx += cxxs * n * v_th * _NN0
+            for j, jtrgt in enumerate(self.targets):
+                if jtrgt.cx: # Only compute cx with target gas if wished by user
+                    cxxs  = 1.43e-16 * q**1.17 * jtrgt.ip**-2.76
+                    R_cx += cxxs * n * v_th * y[self.lb[j]]
+            dn       -= R_cx
+            dn[:-1]  += R_cx[1:]
+            dkT[:-1] += R_cx[1:] / n[:-1] * (kT[1:] - kT[:-1])
+
+
+            # Axial escape
+            # TODO: Check axial energy escapre rate
+            R_ax      = plasma.escape_rate_axial(n, kT, _RI, self.device.v_ax)
+            dn       -= R_ax
+            dkT      -= R_ax / n * q * self.device.v_ax
+
+
+            # Radial escape
+            # TODO: Check radial energy escapre rate
+            R_ra      = plasma.escape_rate_radial(
+                n, kT, _RI, trgt.A, self.device.v_rad, self.device.b_ax, self.device.r_dt
+            )
+            dn       -= R_ra
+            dkT      -= R_ra / n * q * (
+                self.device.v_ra
+                + self.device.r_dt * self.device.b_ax * np.sqrt(2 * Q_E * kT / (3 * trgt.A *M_P))
+            )
+
+
+            # Electron heating / Spitzer heating
+            dkT += plasma.electron_heating_vec(n, ne, kT, self.device.e_kin, trgt.A)
+
+
+            #Check if neutrals are depletable or if there is continuous neutral injection
+            if trgt.cni:
+                dn[0] = 0.0
+                dkT[0] = 0.0
+
+
+            # Fill rates into output vectors
+            dn_out[lb, mb] = dn
+            dkT_out[mb, ub] = dkT
+        return np.concatenate((dn_out, dkT_out))
+
+    def jac(self, t, y):
+        pass
